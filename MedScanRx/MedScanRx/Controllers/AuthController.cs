997eba18859cf6bb0390db45257fda15e3083626
Chris Andrews <chris.chase.andrews@gmail.com>
2018-08-12 22:02:59 -0600

    Got a barebones Auth test working. Need to figure out DB schema but this is what Im going to use.

---
+++ MedScanRx/MedScanRx/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using MedScanRx.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MedScanRx.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Auth/")]
+    public class AuthController : Controller
+    {
+        IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("Token")]
+        public IActionResult Token([FromBody]LoginRequest request)
+        {
+            if (request.UserName == "Chris" && request.Password == "wow")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.UserName),
+                    new Claim("AdminClaimTest", "")
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "medscanrx.com",
+                    audience: "medscanrx.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+
+            return BadRequest(new { error = "Invalid login request" });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("TestAuth")]
+        public IActionResult TestAuth()
+        {
+            return Ok("Testing this is authorized");
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpGet]
+        [Route("TestAdmin")]
+        public IActionResult TestAdmin()
+        {
+            var x = this;
+            return Ok("Testing this is authorized");
+        }
+
+    }
+}
--- MedScanRx/MedScanRx/Startup.cs
+++ MedScanRx/MedScanRx/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace MedScanRx
 {
@@ -24,6 +27,27 @@ namespace MedScanRx
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "medscanrx.com",
+                        ValidAudience = "medscanrx.com",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", policy => policy.RequireClaim("AdminClaimTest"));
+            });
+
             services.AddCors();
 
             services.AddSingleton(Configuration);
@@ -48,6 +72,8 @@ namespace MedScanRx
             app.UseCors(builder => builder.WithOrigins(new string[] { "http://localhost:3000",
                 "http://medscanrxtesting.s3-website-us-west-1.amazonaws.com" }
             ).AllowAnyMethod().AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
